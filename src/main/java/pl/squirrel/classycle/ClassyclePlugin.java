2095853120bb2f36f5dc03db4af2d2429adebf76
Andrey Agafonov <aagafonov@openmobileww.com>
2017-03-22 15:28:44 +0300

    Added Android projects support. Major refactoring.

--- src/main/java/pl/squirrel/classycle/ClassyclePlugin.java
+++ src/main/java/pl/squirrel/classycle/ClassyclePlugin.java
@@ -1,97 +1,110 @@
 package pl.squirrel.classycle;
 
-import classycle.ant.DependencyCheckingTask;
-import org.apache.tools.ant.types.FileSet;
-import org.gradle.api.Action;
+import com.android.build.gradle.AppExtension;
+import com.android.build.gradle.api.AndroidSourceSet;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.reporting.ReportingExtension;
 import org.gradle.api.tasks.SourceSet;
-import org.gradle.internal.UncheckedException;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.SortedMap;
 
+/**
+ * Gradle plugin that creates classycle tasks for project source sets. Task names are built as
+ * "classycle" + source set name (e.g. "classycleRelease", "classycleMain" etc).
+ *
+ * Resulting task graph is:
+ * -- check
+ * ---- classycle
+ * ------ classycleRelease
+ * ------ classycleDebug
+ * ...
+ *
+ * Specify classycle definition file path to enable check for the particular source set (e.g.
+ * classycleRelease.definitionFilePath = "config/classycle.txt")
+ *
+ * If definition file is not specified the check is skipped for the particular source set.
+ */
 public class ClassyclePlugin implements Plugin<Project> {
 
     @Override
     public void apply(final Project project) {
-        final Logger log = project.getLogger();
-        final JavaPluginConvention javaPlugin = project.getConvention().getPlugin(JavaPluginConvention.class);
-        final ReportingExtension reporting = project.getExtensions().getByType(ReportingExtension.class);
-        final SortedMap<String, SourceSet> sourceSets = javaPlugin.getSourceSets().getAsMap();
+        final Logger logger = project.getLogger();
 
         final Task classycleTask = project.task("classycle");
-        final Task checkTask = project.getTasks().getByName("check");
-        for (String name : sourceSets.keySet()) {
-            final SourceSet sourceSet = sourceSets.get(name);
-            final String taskName = sourceSet.getTaskName("classycle", null);
-            final SourceSetClassycleTask task = project.getTasks().create(taskName, SourceSetClassycleTask.class);
 
-            final File definitionFile;
-            if (task.getDefinitionFilePath() != null) {
-                definitionFile = project.file(task.getDefinitionFilePath());
-                if (!definitionFile.exists()) {
-                    throw new RuntimeException("Definition file not found: " + definitionFile);
-                }
-            } else {
-                definitionFile = project.file("src/test/resources/classycle-" + name + ".txt");
-                if (!definitionFile.exists()) {
-                    log.debug("Default classycle definition file not found: " + definitionFile + ", skipping source set "
-                                  + name);
-                    continue;
-                }
-            }
+        final JavaPluginConvention javaPlugin = project.getConvention().getPlugin(JavaPluginConvention.class);
+        for (SourceSet sourceSet : javaPlugin.getSourceSets()) {
+            logger.debug("Creating classycle tasks for Java source sets");
 
-            final File classDir = sourceSet.getOutput().getClassesDir();
-            final File reportFile = reporting.file("classycle/" + name + ".txt");
-            task.getInputs().files(classDir, definitionFile);
-            task.getOutputs().file(reportFile);
-            log.debug("Created classycle task: " + taskName + ", report file: " + reportFile);
-            task.doLast(new Action<Task>() {
-                @Override
-                public void execute(Task task) {
-                    if (!classDir.isDirectory()) {
-                        log.debug("Class directory doesn't exist, skipping: " + classDir);
-                        return;
-                    }
-                    reportFile.getParentFile().mkdirs();
-                    try {
-                        log.debug("Running classycle analysis on: " + classDir);
-                        DependencyCheckingTask classycle = new DependencyCheckingTask();
-                        classycle.setReportFile(reportFile);
-                        classycle.setFailOnUnwantedDependencies(true);
-                        classycle.setMergeInnerClasses(true);
-                        classycle.setDefinitionFile(definitionFile);
-                        classycle.setProject(project.getAnt().getAntProject());
-                        FileSet fileSet = new FileSet();
-                        fileSet.setIncludes("**/*.class");
-                        fileSet.setDir(classDir);
-                        fileSet.setProject(classycle.getProject());
-                        classycle.add(fileSet);
-                        classycle.execute();
-                    } catch (Exception e) {
-                        throw new RuntimeException(
-                            "Classycle check failed: " + e.getMessage() + ". See report at "
-                                + clickableFileUrl(reportFile), e);
-                    }
-                }
-            });
-            classycleTask.dependsOn(task);
-            checkTask.dependsOn(classycleTask);
+            createSourceSetClassycleTask(logger,
+                                         classycleTask,
+                                         sourceSet.getName(),
+                                         sourceSet.getOutput().getClassesDir(),
+                                         sourceSet.getClassesTaskName());
         }
+
+        final AppExtension androidExtension = project.getExtensions().findByType(AppExtension.class);
+        if (androidExtension != null) {
+            logger.debug("Creating classycle tasks for Android source sets");
+
+            for (AndroidSourceSet sourceSet : androidExtension.getSourceSets()) {
+                final File classesDir = new File(project.getBuildDir(),
+                                                 "intermediates/classes/" + sourceSet.getName());
+                createSourceSetClassycleTask(logger,
+                                             classycleTask,
+                                             sourceSet.getName(),
+                                             classesDir,
+                                             "assemble");
+            }
+        }
+
+        project.getTasks().getByName("check").dependsOn(classycleTask);
     }
 
-    private String clickableFileUrl(File path) {
-        try {
-            return (new URI("file", "", path.toURI().getPath(), (String) null, (String) null)).toString();
-        } catch (URISyntaxException var3) {
-            throw UncheckedException.throwAsUncheckedException(var3);
+    /**
+     * Creates source set classycle task. Added dependency of the created task to general "classycle"
+     * task.
+     *
+     * @param logger          logger
+     * @param classycleTask   general classycle task
+     * @param sourceSetName   source set name
+     * @param classesDir      source set classes directory
+     * @param classesTaskName task that builds class-files
+     */
+    private void createSourceSetClassycleTask(final Logger logger,
+                                              final Task classycleTask,
+                                              final String sourceSetName,
+                                              final File classesDir,
+                                              final String classesTaskName) {
+        final Project project = classycleTask.getProject();
+        final String taskName = "classycle" + capitalizeFirstLetter(sourceSetName);
+        final SourceSetClassycleTask task = project.getTasks().create(taskName, SourceSetClassycleTask.class);
+        task.setSourceSetName(sourceSetName);
+        task.setClassesDir(classesDir);
+        task.dependsOn(project.getTasks().getByName(classesTaskName));
+        classycleTask.dependsOn(task);
+
+        logger.debug("Created task " + taskName
+                             + " for source set " + sourceSetName
+                             + " (classes dir " + classesDir.getAbsolutePath()
+                             + ", classes task name " + classesTaskName
+                             + ")");
+    }
+
+    /**
+     * Capitalizes string first letter.
+     *
+     * @param original original string
+     * @return string with the first letter capitalized
+     */
+    private static String capitalizeFirstLetter(String original) {
+        if (original == null || original.isEmpty()) {
+            return original;
         }
+        return original.substring(0, 1).toUpperCase() + original.substring(1);
     }
 }
--- src/main/java/pl/squirrel/classycle/SourceSetClassycleTask.java
+++ src/main/java/pl/squirrel/classycle/SourceSetClassycleTask.java
@@ -1,12 +1,89 @@
 package pl.squirrel.classycle;
 
+import org.apache.tools.ant.types.FileSet;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.reporting.ReportingExtension;
 
-public class SourceSetClassycleTask extends DefaultTask {
+import java.io.File;
 
+import classycle.ant.DependencyCheckingTask;
+
+/**
+ * Source set classycle check task.
+ */
+class SourceSetClassycleTask extends DefaultTask {
+    /** Reporting extension. */
+    private final ReportingExtension reporting;
+
+    /** Definition file path. */
     private String definitionFilePath;
 
-    public String getDefinitionFilePath() {
+    /** Directory where class-files are stored. */
+    private File classesDir;
+
+    /** Report file. */
+    private File reportFile;
+
+    public SourceSetClassycleTask() {
+        final Logger log = getProject().getLogger();
+
+        reporting = getProject().getExtensions().getByType(ReportingExtension.class);
+
+        doLast(task ->
+               {
+                   if (getDefinitionFilePath() == null) {
+                       log.info("Classycle definition file for task " + task.getName() + " is not specified");
+                       return;
+                   }
+
+                   final File definitionFile = getProject().file(getDefinitionFilePath());
+                   if (!definitionFile.exists()) {
+                       throw new RuntimeException("Classycle definition file "
+                                                          + definitionFile.getAbsolutePath()
+                                                          + " does not exist");
+                   }
+
+                   if (!classesDir.exists() || !classesDir.isDirectory()) {
+                       throw new RuntimeException("Invalid classycle directory " + classesDir.getAbsolutePath());
+                   }
+
+                   reportFile.getParentFile().mkdirs();
+                   try {
+                       log.debug("Running classycle analysis on: " + classesDir);
+
+                       final DependencyCheckingTask depCheckTask = new DependencyCheckingTask();
+                       depCheckTask.setReportFile(reportFile);
+                       depCheckTask.setFailOnUnwantedDependencies(true);
+                       depCheckTask.setMergeInnerClasses(true);
+                       depCheckTask.setDefinitionFile(definitionFile);
+                       depCheckTask.setProject(getProject().getAnt().getAntProject());
+                       final FileSet fileSet = new FileSet();
+                       fileSet.setIncludes("**/*.class");
+                       fileSet.setDir(classesDir);
+                       fileSet.setProject(depCheckTask.getProject());
+                       depCheckTask.add(fileSet);
+                       depCheckTask.execute();
+                   }
+                   catch (Exception e) {
+                       throw new RuntimeException("Classycle check failed: " + e.getMessage()
+                                                          + ". See report at " + reportFile.getAbsolutePath());
+
+                   }
+               });
+    }
+
+    void setSourceSetName(final String sourceSetName) {
+        reportFile = reporting.file("classycle/" + sourceSetName + ".txt");
+        getOutputs().file(reportFile);
+    }
+
+    void setClassesDir(final File classesDir) {
+        this.classesDir = classesDir;
+        getInputs().files(classesDir);
+    }
+
+    private String getDefinitionFilePath() {
         return definitionFilePath;
     }
 
